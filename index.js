a8be12c860a86beb1c528f92f899bbe93e098dbc
Michael Lyons <michael.lyons1112@gmail.com>
2023-11-14 19:40:41 +0000

    Adding location search

--- index.js
+++ index.js
@@ -5,35 +5,55 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-const API_URL = "https://api.open-meteo.com/v1/forecast";
-let lat = 53.43;
-let lon = -2.91;
+const OpenMeteo_URL = "https://api.open-meteo.com/v1/forecast";
+const Nominatim_URL = "https://nominatim.openstreetmap.org/search";
 
 const weatherCodeDict = {0: "images/icons/weather/animated/clear-day.svg",
                          1: "images/icons/weather/animated/clear-day.svg",
                          2: "images/icons/weather/animated/clear-day.svg",
                          3: "images/icons/weather/animated/cloudy.svg",
+                         45: "images/icons/weather/animated/fog.svg",
+                         48: "images/icons/weather/animated/fog.svg",
                          51: "images/icons/weather/animated/rainy-1.svg",
                          53: "images/icons/weather/animated/rainy-1.svg",
+                         55: "images/icons/weather/animated/rainy-1.svg",
                          61: "images/icons/weather/animated/rainy-3.svg",
+                         63: "images/icons/weather/animated/rainy-3.svg",
                          80: "images/icons/weather/animated/rainy-2.svg"};
 
+const location = [53.43, -2.91, 'Liverpool'];
+
 app.get("/", async (req, res) => {
-  const configA = { params: { latitude: lat, longitude: lon, daily: ['sunrise', 'sunset', 'temperature_2m_max', 'temperature_2m_min', 'apparent_temperature_max', 'apparent_temperature_min', 'rain_sum', 'weather_code'], forecast_days: 8 } };
-  const configB = { params: { latitude: lat, longitude: lon, hourly: ['apparent_temperature', 'precipitation_probability', 'weather_code'], forecast_days: 1 } };
   try {
-    const sevenDayForecast = await axios.get(API_URL, configA);
-    // const todayForecast = await axios.get(API_URL, configB);
+    let OpenMeteoConfigA = { params: { latitude: location[0], longitude: location[1], daily: ['sunrise', 'sunset', 'temperature_2m_max', 'temperature_2m_min', 'apparent_temperature_max', 'apparent_temperature_min', 'rain_sum', 'weather_code'], forecast_days: 8 } };
+    let OpenMeteoConfigB = { params: { latitude: location[0], longitude: location[1], hourly: ['apparent_temperature', 'precipitation_probability', 'weather_code'], forecast_days: 1 } };
+    let sevenDayForecast = await axios.get(OpenMeteo_URL, OpenMeteoConfigA);
+    // const todayForecast = await axios.get(OpenMeteo_URL, OpenMeteoConfigB);
     //let sunrise_time = new Date(result.data.daily.sunrise[0]).toLocaleString();
-    console.log(sevenDayForecast.data);
+    // console.log(sevenDayForecast.data);
     // console.log(todayForecast.data);
 
-    res.render("index.ejs", { sevenDay: sevenDayForecast.data, weatherCodeDict: weatherCodeDict })
+    res.render("index.ejs", { sevenDay: sevenDayForecast.data, weatherCodeDict: weatherCodeDict, setLocation: location[2] })
   } catch (error) {
-    res.render("index.ejs")
+    res.redirect("/");
   }
 });
+
+app.post("/submit", async (req, res) => {
+	  try {
+      let NominatimConfig = { params: { q: req.body.search, format: "geojson" } };
+      let tmpLocation = await axios.get(Nominatim_URL, NominatimConfig);
+      location[0] = tmpLocation.data.features[0].geometry.coordinates[1];
+      location[1] = tmpLocation.data.features[0].geometry.coordinates[0];
+      const place = tmpLocation.data.features[0].properties.display_name
+      location[2] = place.slice(0, place.indexOf(",")) + place.slice(place.indexOf(",") + 1, place.indexOf(",", place.indexOf(",") + 1))
+      res.redirect("/");
+	  } catch (error) {
+      res.redirect("/");
+	  }
+	});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
